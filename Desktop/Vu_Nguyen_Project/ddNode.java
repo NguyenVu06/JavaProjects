f795bb703ede2dddf0c29553a0598447db4bd025
NguyenVu06 <dnvdcap87@gmail.com>
2020-07-01 22:42:56 -0400

    added Desktop/Vu_Nguyen_Project/ddNode.java

---
+++ Desktop/Vu_Nguyen_Project/ddNode.java
@@ -0,0 +1,17 @@
+
+package project_19;
+
+public class ddNode
+{
+    private final int key;
+    private final int value;
+
+    public ddNode(int aKey, int aValue)
+    {
+        key   = aKey;
+        value = aValue;
+    }
+
+    public int key()   { return key; }
+    public int value() { return value; }
+}
